b47e7785509d36a6cdcddccd88b3849b047d4e34
Michael Lopez <llopez1@fandm.edu>
2018-02-24 11:42:03 -0800

    Added more information on location!

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function clickedbutton() {
+
+var response = prompt("Do you need to find a restroom?");
+
+function clickedButton() {
     document.getElementById("restroomlist").classList.remove("hide")  
 }
+document.getElementById("restroomButton").addEventListener("click", clickedButton);
 
-document.getElementById("restroomButton").addEventListener("click", clickedbutton);
+function showrestroom () {
+    document.getElementById("showthis").classList.remove("hide")
+    }
+    document.getElementById("toilet").addEventListener("click", showrestroom);
 
+
+
+
+// IMMEDIATE NEXT ACTIONABLE STEPS
+// next stepts: in the HTML create some paragraphs and add a class to hide the paragraphs, like we did today. 
+// And then each method, be sure you targetting the right paragraph to show
+// Look at ways to write less code
+// Think about ways you can store variables
+// If you write something more than once, it should be a variable or a function. 
